35ca7f9fe215107389c0c26f3a8daaa007e2b4ee
Oliver Turner <oliver.turner@codedsignal.co.uk>
2018-06-23 11:09:15 +0100

    Minor issues with eslint continue...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   env: {
     browser: true,
+    node: true,
     es6: true,
     jest: true
   },
-  // parserOptions: { sourceType: "module" },
-  extends: ["esnext", "plugin:react/recommended"],
+  parserOptions: { sourceType: "module" },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   plugins: ["react"],
   rules: {
     "no-console": "off"
